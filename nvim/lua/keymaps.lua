bb26c1c5ff67cbed4868b2b3f72019e05e5cc1ea
Michael.Krebs <michael.krebs@xpublisher.com>
2023-06-19 16:12:01 +0200

    Update nvim/lua/keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,17 +1,21 @@
 vim.g.mapleader = " "
+-- Telescope mappings in after/plugin/telescope.lua
+
+-----------------
+-- Normal mode --
+-----------------
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>pb", vim.cmd.bprevious)
 vim.keymap.set("n", "<leader>nb", vim.cmd.bnext)
 vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
 
--- Telescope mappings in after/plugin/telescope.lua
-
------------------
--- Normal mode --
------------------
-
 -----------------
 -- Visual mode --
 -----------------
+
+-----------------
+-- Insert mode --
+-----------------
+vim.keymap.set("i", "<C-H>", "<C-W>")
 
