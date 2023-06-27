bae7e09dbc228fad33733ceb57c61411260b4853
Michael Krebs <michikrbs@gmail.com>
2023-06-27 14:33:47 +0200

    Update nvim/lua/keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,14 +1,22 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 -- Telescope mappings in after/plugin/telescope.lua
 
 -----------------
 -- Normal mode --
 -----------------
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", "<leader>pb", vim.cmd.bprevious)        -- Switches to previous Buffer
-vim.keymap.set("n", "<leader>nb", vim.cmd.bnext)            -- Switches to next Buffer
-vim.keymap.set("n", "<leader>bd", vim.cmd.bd)               -- Closes current Buffer
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<C-n>', vim.cmd.NvimTreeToggle)
+vim.keymap.set('n', '<leader>pb', vim.cmd.bprevious)        -- Switches to previous Buffer
+vim.keymap.set('n', '<leader>nb', vim.cmd.bnext)            -- Switches to next Buffer
+vim.keymap.set('n', '<leader>bd', vim.cmd.bd)               -- Closes current Buffer
+
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+
 
 -----------------
 -- Visual mode --
@@ -17,4 +25,4 @@ vim.keymap.set("n", "<leader>bd", vim.cmd.bd)               -- Closes current Bu
 -----------------
 -- Insert mode --
 -----------------
-vim.keymap.set("i", "<C-H>", "<C-W>")                       -- Adds ctrl+backspace functionality in insert mode
+vim.keymap.set('i', '<C-H>', '<C-W>')                       -- Adds ctrl+backspace functionality in insert mode
