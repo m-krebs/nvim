e6f8cb61f4dc0b6b34d3eab422957bd628594732
Michael Krebs <michikrbs@gmail.com>
2023-09-10 20:06:53 +0200

    update keymaps and options

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,8 +1,7 @@
-vim.g.mapleader = ' '
 vim.g.floaterm_keymap_new    = '<F7>'
 vim.g.floaterm_keymap_toggle = '<F12>'
 
--- Telescope mappings in after/plugin/telescope.lua
+-- Telescope mappings at after/plugin/telescope.lua
 
 -----------------
 -- Normal mode --
@@ -13,19 +12,37 @@ vim.keymap.set('n', '<leader>pb', vim.cmd.bprevious)        -- Switches to previ
 vim.keymap.set('n', '<leader>nb', vim.cmd.bnext)            -- Switches to next Buffer
 vim.keymap.set('n', '<leader>bd', vim.cmd.bd)               -- Closes current Buffer
 
+vim.keymap.set("n", "J", "mzJ`z") -- Keep cursor when appending below line
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 -- Better window navigation
 vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
 vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
 vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
 vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 
-
-
 -----------------
 -- Visual mode --
 -----------------
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
 -----------------
 -- Insert mode --
 -----------------
 vim.keymap.set('i', '<C-H>', '<C-W>')                       -- Adds ctrl+backspace functionality in insert mode
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- add to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -8,13 +8,31 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+
 -- UI config
+vim.opt.wrap = false
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 
+vim.opt.scrolloff = 8
+
 -- Searching
 vim.opt.hlsearch = false
+vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- History
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- Other
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
