a8efd04ae6cdefe5bf7914c2e9939988d80f6789
Michael Krebs <michikrbs@gmail.com>
2023-09-10 20:05:48 +0200

    add additional plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,18 +21,33 @@ return require('packer').startup(function(use)
 	use { 'lukas-reineke/indent-blankline.nvim', requires = 'nvim-treesitter/nvim-treesitter' }
 	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { { 'nvim-lua/plenary.nvim' } } }
 	use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
-    use { 'williamboman/mason.nvim', { run = ':MasonUpdate' } }
-    use { 'williamboman/mason-lspconfig.nvim' }
-    use { 'neovim/nvim-lspconfig' }
-    use { 'hrsh7th/nvim-cmp' }
-    use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' }
-    use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' }        -- buffer auto-completio
-    use { 'hrsh7th/cmp-path', after = 'nvim-cmp' }          -- path auto-completion
-    use { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' }       -- cmdline auto-completion
-    use { 'L3MON4D3/LuaSnip' }
-    use { 'saadparwaiz1/cmp_luasnip' }
     use { 'towolf/vim-helm' }
     use { 'voldikss/vim-floaterm' }
+    use { 'theprimeagen/harpoon' }
+    use { 'mbbill/undotree' }
+    use { 'tpope/vim-fugitive' }
+    use { 'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig'},             -- Required
+            { 'williamboman/mason.nvim', { run = ':MasonUpdate' } },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lua' }, -- Required
+            { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },   -- Optional
+            { 'hrsh7th/cmp-path', after = 'nvim-cmp' },     -- Optional
+            { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' },  -- Optional
+            { 'saadparwaiz1/cmp_luasnip' },                 -- Optional
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 
  if packer_bootstrap then
     require('packer').sync()
