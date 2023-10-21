5ee9631d01241b771d1dc2d77348d105d4c36b6a
Michael Krebs <michikrbs@gmail.com>
2023-10-21 16:20:57 +0200

    chore(prettier): update config

--- after/plugin/neoformat.lua
+++ after/plugin/neoformat.lua
@@ -2,3 +2,9 @@ vim.g.neoformat_html_prettier = {
     exe = 'prettier',
     args = { '--parser html', '--bracket-same-line true', '--html-whitespace-sensitivity ignore' }
 }
+--[[
+vim.g.neoformat_typescript_prettier = {
+    exe = 'prettier',
+    args = { '--parser typescript', '--tab-width 4' }
+}
+]]
