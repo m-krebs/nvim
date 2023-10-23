15e493aba152c96665567956770fa5a5dd81612c
Michael Krebs <michikrbs@gmail.com>
2023-10-23 23:03:15 +0200

    feat(keymaps): backspace delete

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+      })
+    end,
+  },
+}
