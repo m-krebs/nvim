b8503d1f61d7f2c7ba2f25a93058a345d3ab8860
Michael Krebs <michikrbs@gmail.com>
2023-10-25 20:59:49 +0200

    keymaps: update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- ctrl+backspace delete previous word
 vim.keymap.set("i", "<C-BS>", "<C-W>")
 vim.keymap.set("i", "<C-H>", "<C-W>")
+
+-- make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
