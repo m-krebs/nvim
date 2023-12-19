c19fefa065b33bd25129dade4f17bd6cecb46e5e
Michael.Krebs <michael.krebs@xpublisher.com>
2023-12-19 14:33:07 +0100

    plugin: telescope keymap git_files

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      {
+        "<leader>fg",
+        "<cmd>Telescope git_files<cr>",
+        desc = "Find git Files",
+      },
+    },
+  },
+}
