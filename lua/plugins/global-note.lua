518409b6a7e446f16843b8653479344cc9d18811
Michael Krebs <michikrbs@gmail.com>
2024-02-15 19:49:42 +0100

    plugin: add global-note

---
+++ lua/plugins/global-note.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "backdround/global-note.nvim",
+    keys = {
+      {
+        "<leader>n",
+        function()
+          require("global-note").toggle_note()
+        end,
+        desc = "Toggle global note",
+      },
+    },
+  },
+}
