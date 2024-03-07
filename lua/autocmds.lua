93378b19f930029475ba9d47581d79a284ddad47
Michael Krebs <michikrbs@gmail.com>
2024-03-07 19:23:13 +0100

    autocmds: fix augroup

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,3 +1,9 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+local function augroup(name)
+  return vim.api.nvim_create_augroup("myvim_" .. name, { clear = true })
+end
+
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd('BufReadPost', {
   group = augroup 'last_loc',
