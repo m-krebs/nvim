151690826743fd95f176255edad5d5869fb24aa6
Michael Krebs <michikrbs@gmail.com>
2024-03-07 01:42:43 +0100

    init kickstart

---
+++ lua/options.lua
@@ -0,0 +1,55 @@
+-- See `:help vim.opt or option-list`
+local opt = vim.opt
+
+-- Enable relative line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in status line
+opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  See `:help 'clipboard'`
+opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+opt.breakindent = true
+
+-- Save undo history
+opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = 'yes'
+
+-- Decrease update time
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+opt.splitright = true
+opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+opt.hlsearch = true
