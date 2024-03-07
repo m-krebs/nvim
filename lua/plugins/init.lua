28a3f6bff53e92ac736b870d826d60b9268e7fcc
Michael Krebs <michikrbs@gmail.com>
2024-03-07 19:07:59 +0100

    keymaps: update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -426,11 +426,6 @@ return {
               luasnip.expand_or_jump()
             end
           end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
         },
         sources = {
           { name = 'nvim_lsp' },
