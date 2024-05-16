622c50f7022b154b539fae67febd06de9fb0619f
Michael Krebs <michikrbs@gmail.com>
2024-05-16 21:15:21 +0200

    add json ft formatter (prettier)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,6 +28,7 @@ return {
       },
       formatters_by_ft = {
         lua = { 'stylua' },
+        json = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
