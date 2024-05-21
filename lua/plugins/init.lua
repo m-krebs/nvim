7413ebfadade6426c3e4e2472fc94311093f6440
Michael Krebs <michikrbs@gmail.com>
2024-05-21 13:54:02 +0200

    plugin(cmp): add scroll_docs keymap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -93,6 +93,8 @@ return {
         mapping = cmp.mapping.preset.insert {
           ['<C-k>'] = cmp.mapping.select_prev_item(),
           ['<C-j>'] = cmp.mapping.select_next_item(),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
           ['<CR>'] = cmp.mapping.confirm { select = true },
 
