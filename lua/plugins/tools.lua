add7b89e0c05df6ce3dc25ff1b23fbb2539d8d3b
Michael Krebs <michikrbs@gmail.com>
2024-05-21 14:01:46 +0200

    plugins: add hawtkeys

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -13,4 +13,12 @@ return {
       },
     },
   },
+  {
+    'tris203/hawtkeys.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {},
+  },
 }
