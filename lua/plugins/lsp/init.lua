956af558cb30f3d15470e7d08cda03810eab2690
Michael Krebs <michikrbs@gmail.com>
2024-07-04 18:37:05 +0200

    add icons to mason

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -184,5 +184,14 @@ return {
     cmd = 'Mason',
     keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
     build = ':MasonUpdate',
+    opts = {
+      ui = {
+        icons = {
+          package_installed = '',
+          package_uninstalled = '',
+          package_pending = '',
+        },
+      },
+    },
   },
 }
