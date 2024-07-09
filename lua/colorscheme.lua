3667563984f979df082214bf5b21b03802c95454
Michael Krebs <michikrbs@gmail.com>
2024-07-09 19:02:12 +0200

    same as before

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -2,5 +2,5 @@ local colorscheme = 'ayu-mirage'
 
 local is_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 if not is_ok then
-  print('colorscheme ' .. colorscheme .. ' not found!')
+  vim.notify('colorscheme ' .. colorscheme .. ' not found!', vim.log.levels.ERROR)
 end
