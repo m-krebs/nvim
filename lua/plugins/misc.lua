29d5c17ee13375e3bd1cac376e420abc530a0b1e
Michael Krebs <michikrbs@gmail.com>
2024-07-15 13:02:04 +0200

    update rest-client config

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -11,7 +11,19 @@ return {
       { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
   },
-  { 'mistweaverco/kulala.nvim' }, -- HTTP REST-Client Interface
+  {
+    'mistweaverco/kulala.nvim', -- HTTP REST-Client Interface
+    lazy = false,
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      {"<leader>r", "", desc = "+[R]est"},
+      {"<leader>rr", function () require('kulala').run() end, desc = 'Run request'},
+      {"<leader>r[", function () require('kulala').jump_prev() end, desc = 'Jump to previous'},
+      {"<leader>r]", function () require('kulala').jump_next() end, desc = 'Jump to next'},
+      {"<leader>rv", function () require('kulala').toggle_view() end, desc = 'Toggle view'}
+    },
+  },
   {
     'MeanderingProgrammer/markdown.nvim',
     dependencies = {
