4f776f58d290b74cd03cd8af28e122ff3845b777
Michael Krebs <michikrbs@gmail.com>
2024-07-15 13:01:44 +0200

    add map local for vim.keymap.set

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,47 +1,58 @@
+local map = vim.keymap.set
+
 --  See `:help vim.keymap.set()`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Editor
 -- ctrl+backspace delete previous word
-vim.keymap.set('i', '<C-BS>', '<C-W>')
-vim.keymap.set('i', '<C-H>', '<C-W>')
-vim.keymap.set('i', 'jk', '<Esc>')
+map('i', '<C-BS>', '<C-W>')
+map('i', '<C-H>', '<C-W>')
+map('i', 'jk', '<Esc>')
 
 -- void
-vim.keymap.set('n', 'Q', '"_', { desc = 'Start void register' })
+map('n', 'Q', '"_', { desc = 'Start void register' })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>cd', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+map('n', '<leader>cd', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- tabs
+map('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First tab' })
+map('n', '<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last tab' })
+map('n', '<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'New tab' })
+map('n', '<leader><tab>c', '<cmd>tabclose<cr>', { desc = 'Close tab' })
+map('n', '<leader><tab>o', '<cmd>tabonly<cr>', { desc = 'Close other tabs' })
+map('n', '<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Next tab' })
+map('n', '<leader><tab>[', '<cmd>tabprevious<cr>', { desc = 'Previous tab' })
 
 -- windows `:help wincmd`
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-vim.keymap.set('n', '<leader>wd', '<C-w>c', { desc = 'Close current window' })
-vim.keymap.set('n', '<leader>|', '<cmd>vsplit<cr>', { desc = 'Split window horizontally' })
-vim.keymap.set('n', '<leader>-', '<cmd>split<cr>', { desc = 'Split window vertically' })
-vim.keymap.set('n', '<leader>w|', '<cmd>vsplit<cr>', { desc = 'Split window horizontally' })
-vim.keymap.set('n', '<leader>w-', '<cmd>split<cr>', { desc = 'Split window vertically' })
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<leader>wd', '<C-w>c', { desc = 'Close current window' })
+map('n', '<leader>|', '<cmd>vsplit<cr>', { desc = 'Split window horizontally' })
+map('n', '<leader>-', '<cmd>split<cr>', { desc = 'Split window vertically' })
+map('n', '<leader>w|', '<cmd>vsplit<cr>', { desc = 'Split window horizontally' })
+map('n', '<leader>w-', '<cmd>split<cr>', { desc = 'Split window vertically' })
 
 -- buffers
-vim.keymap.set('n', '<leader>bb', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
-vim.keymap.set('n', '<leader>`', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
+map('n', '<leader>bb', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
+map('n', '<leader>`', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
 
-vim.keymap.set('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
+map('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
 
 -- terminal
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Normal mode in terminal' })
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Normal mode in terminal' })
 
 -- lazy
-vim.keymap.set('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
+map('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 
 -- quit
-vim.keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
+map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
 
 -- Copilot
-vim.keymap.set('i', '<Tab>', '<Plug>(copilot-accept-word)')
-vim.keymap.set('i', '<C-L>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+map('i', '<Tab>', '<Plug>(copilot-accept-word)')
+map('i', '<C-L>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
