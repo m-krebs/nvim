57571d2c5e8a15f1ebc855aba58f832ee72ce948
Michael Krebs <michikrbs@gmail.com>
2024-08-21 10:48:22 +0200

    autocmds: autocommit lazylock

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,6 +4,8 @@ local function augroup(name)
   return vim.api.nvim_create_augroup('myvim_' .. name, { clear = true })
 end
 
+local autocmd = vim.api.nvim_create_autocmd
+
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd('BufReadPost', {
   group = augroup 'last_loc',
@@ -28,5 +30,40 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+autocmd('User', {
+  pattern = 'LazyUpdate',
+  callback = function()
+    local repo_dir = '/home/mkrebs/.local/share/chezmoi/lib/external_nvim'
+    local lockfile = repo_dir .. '/lazy-lock.json'
+
+    local cmd = {
+      'git',
+      '-C',
+      repo_dir,
+      'commit',
+      lockfile,
+      '-m',
+      'Update lazy-lock.json',
+    }
+
+    local success, process = pcall(function()
+      return vim.system(cmd):wait()
+    end)
+
+    if process and process.code == 0 then
+      vim.notify 'Commited lazy-lock.json'
+      vim.notify(process.stdout)
+    else
+      if not success then
+        vim.notify("Failed to run command '" .. table.concat(cmd, ' ') .. "':", vim.log.levels.WARN, {})
+        vim.notify(tostring(process), vim.log.levels.WARN, {})
+      else
+        vim.notify 'git ran but failed to commit:'
+        vim.notify(process.stdout, vim.log.levels.WARN, {})
+      end
+    end
   end,
 })
