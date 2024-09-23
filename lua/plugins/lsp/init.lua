524300a5aa0c6f96a1abe19c82e03a7027b1bc29
Michael Krebs <michikrbs@gmail.com>
2024-09-23 21:27:14 +0200

    plugins: add neoconf and move config

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,4 +1,8 @@
 return {
+  {
+    'folke/neoconf.nvim',
+    opts = true,
+  },
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     event = 'BufReadPre',
@@ -109,33 +113,6 @@ return {
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-
-        lua_ls = {
-          -- cmd = {...},
-          -- filetypes { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              runtime = { version = 'LuaJIT' },
-              workspace = {
-                checkThirdParty = false,
-                -- Tells lua_ls where to find all the Lua files that you have loaded
-                -- for your neovim configuration.
-                library = {
-                  '${3rd}/luv/library',
-                  unpack(vim.api.nvim_get_runtime_file('', true)),
-                },
-                -- If lua_ls is really slow on your computer, you can try this instead:
-                -- library = { vim.env.VIMRUNTIME },
-              },
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
       }
 
       -- Ensure the servers and tools above are installed
@@ -191,5 +168,10 @@ return {
         },
       },
     },
+  },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
   },
 }
