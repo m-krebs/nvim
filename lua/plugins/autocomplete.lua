33ff488527fdcb5ef01d42167fdfda65e9668dea
Michael Krebs <michikrbs@gmail.com>
2024-10-02 23:55:05 +0200

    config: nvim-cmp and luasnip

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -10,6 +10,10 @@ return {
       -- Snippet Engine & its associated nvim-cmp source
       {
         'L3MON4D3/LuaSnip',
+        dependencies = {
+          'rafamadriz/friendly-snippets',
+          'onsails/lspkind.nvim',
+        },
         build = (function()
           -- Build Step is needed for regex support in snippets
           -- This step is not supported in many windows environments
@@ -19,12 +23,26 @@ return {
           end
           return 'make install_jsregexp'
         end)(),
-        dependencies = {
-          'rafamadriz/friendly-snippets',
-        },
+        opts = { updateevents = 'TextChanged,TextChangedI' },
+        config = function(_, opts)
+          require('luasnip').config.set_config(opts)
+          -- vscode format
+          require('luasnip.loaders.from_vscode').lazy_load { exclude = vim.g.vscode_snippets_exclude or {} }
+          require('luasnip.loaders.from_vscode').lazy_load { paths = vim.g.vscode_snippets_path or '' }
+
+          vim.api.nvim_create_autocmd('InsertLeave', {
+            callback = function()
+              if require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()] and not require('luasnip').session.jump_active then
+                require('luasnip').unlink_current()
+              end
+            end,
+          })
+        end,
       },
       'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lua',
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
     },
 
@@ -40,7 +58,8 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = { completeopt = 'noselect' },
+        preselect = cmp.PreselectMode.None,
 
         -- Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
@@ -55,14 +74,31 @@ return {
           ['<C-Space>'] = cmp.mapping.complete {},
         },
 
-        ['<C-Space>'] = cmp.mapping.complete {},
-        sources = cmp.config.sources({
+        sources = cmp.config.sources {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
-          { name = 'path' },
-        }, {
           { name = 'buffer' },
-        }),
+          { name = 'nvim_lua' },
+          { name = 'path' },
+        },
+        window = {
+          completion = {
+            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+            col_offset = -3,
+            side_padding = 0,
+          },
+        },
+        formatting = {
+          fields = { 'kind', 'abbr', 'menu' },
+          format = function(entry, vim_item)
+            local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
+            local strings = vim.split(kind.kind, '%s', { trimempty = true })
+            kind.kind = ' ' .. (strings[1] or '') .. ' '
+            kind.menu = '    (' .. (strings[2] or '') .. ')'
+
+            return kind
+          end,
+        },
       }
     end,
   },
