4280e3c50f56141ec4d987416bb075ed9de2db7e
Michael Krebs <michikrbs@gmail.com>
2024-10-02 23:55:28 +0200

    plugins: add markdown-preview

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -43,4 +43,12 @@ return {
     },
     opts = {},
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = 'markdown',
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
 }
