f266bdabe384035724f12e0387d4234d165339ad
Michael Krebs <michikrbs@gmail.com>
2024-11-29 11:19:02 +0100

    fix: workaround neovim/neovim#30985

--- init.lua
+++ init.lua
@@ -21,3 +21,14 @@ _G.Utils = require 'utility'
 require 'config.lazy'
 
 require 'colorscheme'
+
+-- workaround for rust-analyzer server cancelled request
+for _, method in ipairs { 'textDocument/diagnostic', 'workspace/diagnostic' } do
+  local default_diagnostic_handler = vim.lsp.handlers[method]
+  vim.lsp.handlers[method] = function(err, result, context, config)
+    if err ~= nil and err.code == -32802 then
+      return
+    end
+    return default_diagnostic_handler(err, result, context, config)
+  end
+end
