ca86c48b1fa31b05cd3b50d9ebacebc12e6e9888
Michael Krebs <michikrbs@gmail.com>
2024-12-19 17:16:06 +0100

    chore: update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,6 +4,6 @@ docker run -w /root -it --rm alpine:edge sh -uelic '
   apk add git curl wget bash lazygit neovim ripgrep fzf cargo alpine-sdk --update
         git clone https://m-krebs.dev/nvim ~/.config/nvim
         cd ~/.config/nvim
-        git checkout ${NVIM_BRANCH}
         nvim --headless "+Lazy! restore" +qa
+        nvim
 '
