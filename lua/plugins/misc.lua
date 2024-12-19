c4e8302275dc253e99ca2ca92c96426eaae20c45
Michael Krebs <michikrbs@gmail.com>
2024-12-19 17:34:18 +0100

    style: format

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -49,7 +49,7 @@ return {
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = 'markdown',
     build = function()
-      vim.opt.rtp:prepend(vim.fn.stdpath('data') .. '/lazy/markdown-preview.nvim')
+      vim.opt.rtp:prepend(vim.fn.stdpath 'data' .. '/lazy/markdown-preview.nvim')
       vim.fn['mkdp#util#install']()
     end,
   },
