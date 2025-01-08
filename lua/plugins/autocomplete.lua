b40e2f65edcc3ce4a13706f9be935d03c5f14dba
Michael Krebs <michikrbs@gmail.com>
2025-01-08 19:43:20 +0100

    plugins(blink.cmp): update configuration

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -7,9 +7,7 @@ return {
     opts = {
       keymap = {
         preset = 'enter',
-        cmdline = {
-          preset = 'default',
-        },
+        cmdline = {},
       },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
@@ -19,9 +17,33 @@ return {
         nerd_font_variant = 'mono',
       },
       completion = {
+        accept = {
+          auto_brackets = {
+            enabled = true,
+          },
+        },
         menu = {
+          border = 'single',
+          auto_show = function(ctx)
+            return ctx.mode ~= 'cmdline'
+          end,
           draw = {
             columns = { { 'kind_icon', 'label', 'label_description', gap = 1 }, { 'kind' } },
+            components = {
+              kind_icon = {
+                ellipsis = false,
+                text = function(ctx)
+                  local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                  return kind_icon
+                end,
+              },
+            },
+            treesitter = { 'lsp' },
+          },
+        },
+        list = {
+          selection = {
+            preselect = true,
           },
         },
       },
