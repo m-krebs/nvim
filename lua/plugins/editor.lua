34013ddded547dd8e0a533fc86eece8ecc9ac0fe
Michael Krebs <michikrbs@gmail.com>
2025-01-20 12:05:34 +0100

    plugins: enable vim-sleuth

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,5 @@
 return {
-  -- { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
+  { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
   { 'numToStr/Comment.nvim', opts = {}, event = 'BufRead' },
   {
     'folke/flash.nvim',
