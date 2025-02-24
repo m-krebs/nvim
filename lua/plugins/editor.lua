8d2ad43974de3dcbdd5a519c7bdcc261f5380028
Michael Krebs <michikrbs@gmail.com>
2025-02-24 15:15:21 +0100

    chore: add keymap for project search (zoxide)

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -50,6 +50,7 @@ return {
       { '<leader>sd', '<cmd>FzfLua diagnostics_document<CR>', desc = 'Search [d]iagnostics' },
       { '<leader>sc', '<cmd>FzfLua commands<CR>', desc = 'Search [c]ommands' },
       { '<leader>sw', '<cmd>FzfLua grep_cWord<CR>', desc = 'Search current [w]ord' },
+      { '<leader>sp', '<cmd>FzfLua zoxide<CR>', desc = 'Search [p]rojects' },
     },
   },
   -- Pretty diagnostics, references, quickfix, location lists
