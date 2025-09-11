a5f73be81d05c37064df3e627062aa710a99de4c
Michael Krebs <michikrbs@gmail.com>
2025-09-11 14:02:00 +0200

    chore: update statusline
    - add macro record section to lualine.nvim
    - disable unused mini statusline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -59,6 +59,23 @@ return {
         options = {
           globalstatus = true,
         },
+        sections = {
+          lualine_b = {
+            {
+              -- shows macro recording in statusline
+              'macro',
+              fmt = function()
+                local reg = vim.fn.reg_recording()
+                if reg ~= '' then
+                  return 'Recording @' .. reg
+                end
+                return nil
+              end,
+              color = { fg = '#ff9e64' },
+              draw_empty = false,
+            },
+          },
+        },
       }
     end,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
