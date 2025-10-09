3cba0d6baee4d0a52631b05465a21c0f0894ae75
Michael Krebs <michikrbs@gmail.com>
2025-10-09 19:27:07 +0200

    add keymaps for autoformat toggle

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -37,5 +37,23 @@ return {
         -- javascript = { { "prettierd", "prettier" } },
       },
     },
+    keys = {
+      {
+        '<leader>uf',
+        function()
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+          Utils.notify.info((vim.g.disable_autoformat and 'Dis' or 'En') .. 'abled autoformat (global)', { title = 'Option' })
+        end,
+        desc = 'Toggle Auto [F]ormat (Global)',
+      },
+      {
+        '<leader>uF',
+        function()
+          vim.b.disable_autoformat = not vim.b.disable_autoformat
+          Utils.notify.info((vim.b.disable_autoformat and 'Dis' or 'En') .. 'abled autoformat (buffer)', { title = 'Option' })
+        end,
+        desc = 'Toggle Auto [F]ormat (Buffer)',
+      },
+    },
   },
 }
