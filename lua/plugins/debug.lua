2ea4a24108f0c1d6c72bc249275ac1ddaa2e4eae
Michael Krebs <michikrbs@gmail.com>
2026-06-25 12:04:51 +0200

    chore: update mason.nvim source

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -7,7 +7,7 @@ return {
       { 'nvim-neotest/nvim-nio', version = '*' }, -- Is required for nvim-dap-ui
 
       -- Installs the debug adapters for you
-      'williamboman/mason.nvim',
+      'mason-org/mason.nvim',
       {
         'jay-babu/mason-nvim-dap.nvim',
         opts = {
