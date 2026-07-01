b05d7b3273d5479a7821e5aaa936c84a1bd0d31a
Michael Krebs <michikrbs@gmail.com>
2026-07-01 23:01:15 +0200

    move minipairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -109,4 +109,28 @@ return {
       { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "[D]elete Buffer (Force)" },
     },
   },
+  {
+    'echasnovski/mini.pairs',
+    version = '*',
+    event = 'VeryLazy',
+    opts = {
+      mappings = {
+        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\`].', register = { cr = false } },
+      },
+    },
+    keys = {
+      {
+        '<leader>up',
+        function()
+          vim.g.minipairs_disable = not vim.g.minipairs_disable
+          if vim.g.minipairs_disable then
+            Utils.notify.warn('Disabled auto pairs', { title = 'Option' })
+          else
+            Utils.notify.info('Enabled auto pairs', { title = 'Option' })
+          end
+        end,
+        desc = 'Toggle Auto [P]airs',
+      },
+    },
+  },
 }
