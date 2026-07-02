27dde70b8ee6ffa322266c42f0fe6f24689d9f9e
Michael Krebs <michikrbs@gmail.com>
2026-07-02 19:09:39 +0200

    cleanup snacks

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,31 +6,3 @@ require 'plugins.lsp'
 require 'plugins.git'
 require 'plugins.snacks'
 require 'plugins.toggleterm'
-
--- return {
---   {
---     'folke/snacks.nvim',
---     lazy = false,
---     opts = {
---       lazygit = { enabled = true },
---       zen = { enabled = true },
---       scroll = { enabled = true },
---     },
---     keys = {
---       {
---         '<leader>z',
---         function()
---           Snacks.zen()
---         end,
---         desc = 'Toggle [z]en mode',
---       },
---       {
---         '<leader>e',
---         function()
---           Snacks.explorer()
---         end,
---         desc = 'Toggle [e]xplorer',
---       },
---     },
---   },
--- }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,31 +12,3 @@ end, { desc = 'Toggle [z]en mode' })
 vim.keymap.set('n', '<leader>e', function()
   Snacks.explorer()
 end, { desc = 'Toggle [e]xplorer' })
-
--- return {
---   {
---     'folke/snacks.nvim',
---     lazy = false,
---     opts = {
---       lazygit = { enabled = true },
---       zen = { enabled = true },
---       scroll = { enabled = true },
---     },
---     keys = {
---       {
---         '<leader>z',
---         function()
---           Snacks.zen()
---         end,
---         desc = 'Toggle [z]en mode',
---       },
---       {
---         '<leader>e',
---         function()
---           Snacks.explorer()
---         end,
---         desc = 'Toggle [e]xplorer',
---       },
---     },
---   },
--- }
