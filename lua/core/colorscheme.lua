ea0cfef6ddf07fa911e5631d66cb104f8bd480a6
Michael Krebs <michikrbs@gmail.com>
2026-07-02 19:31:44 +0200

    move colorschemes

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,1 +1,12 @@
-vim.pack.add { gh 'scottmckendry/cyberdream.nvim' }
+vim.pack.add {
+  gh 'scottmckendry/cyberdream.nvim',
+  gh 'Shatur/neovim-ayu',
+  gh 'sainnhe/sonokai',
+  gh 'folke/tokyonight.nvim',
+  gh 'eldritch-theme/eldritch.nvim',
+  gh 'rebelot/kanagawa.nvim',
+}
+
+vim.cmd.colorscheme 'cyberdream'
+
+-- function() vim.api.nvim_set_hl(0, 'LineNr', { fg = '#34383f' })
